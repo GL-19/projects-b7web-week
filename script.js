6aa9fb06306a69c088cf863a9e65734ea6daa2e8
GL-19 <loiolagl19@gmail.com>
2021-08-08 01:36:13 -0300

    Refatoração do código

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-const data = new Date();
-let [horas, minutos, segundos] = [data.getHours(), data.getMinutes(), data.getSeconds()];
+
 
 let relogioDigital = document.querySelector(".digital");
+let ponteiroSegundos = document.querySelector(".p_s");
+let ponteiroMinutos = document.querySelector(".p_m");
+let ponteiroHoras = document.querySelector(".p_h");
 
 setInterval(atualizarHorario, 1000);
 
 function atualizarHorario() {
-    segundos++;
-    if(segundos === 60) {
-        segundos = 0;
-        minutos++;
-    }
-    if(minutos === 60) {
-        minutos = 0;
-        horas++;
-    }
-    if(horas === 24) {
-        horas = 0;
-    }
-    atualizarRelogioDigital();
-}
-
-function atualizarRelogioDigital() {
-    let displayHoras, displayMinutos, displaySegundos;
-    displayHoras = horas < 10 ? '0' + horas : horas;
-    displayMinutos = minutos < 10 ? '0' + minutos : minutos;
-    displaySegundos = segundos < 10 ? '0' + segundos : segundos;
+    const data = new Date();
+    let [horas, minutos, segundos] = [data.getHours(), data.getMinutes(), data.getSeconds()];
     
-    relogioDigital.innerHTML = `${displayHoras}:${displayMinutos}:${displaySegundos}`;
+    minutos = minutos < 10 ? '0' + minutos : minutos;
+    segundos = segundos < 10 ? '0' + segundos : segundos;
+    relogioDigital.innerHTML = `${corrigirRelogio(horas)}:${corrigirRelogio(minutos)}:${corrigirRelogio(segundos)}`;
+}
+function corrigirRelogio(variavel) {
+    return variavel < 10 ? '0' + variavel : variavel;
 }
