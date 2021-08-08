b19ae0f7f9a7d63d326f0c3e0eeb424eca578db1
GL-19 <loiolagl19@gmail.com>
2021-08-08 01:40:48 -0300

    refatoração de código

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let relogioDigital = document.querySelector(".digital");
 let ponteiroSegundos = document.querySelector(".p_s");
 let ponteiroMinutos = document.querySelector(".p_m");
@@ -8,13 +6,15 @@ let ponteiroHoras = document.querySelector(".p_h");
 setInterval(atualizarHorario, 1000);
 
 function atualizarHorario() {
-    const data = new Date();
-    let [horas, minutos, segundos] = [data.getHours(), data.getMinutes(), data.getSeconds()];
+    const horarioAtual = new Date();
+    let horas = horarioAtual.getHours(); 
+    let minutos = horarioAtual.getMinutes();
+    let segundos = horarioAtual.getSeconds();
     
     minutos = minutos < 10 ? '0' + minutos : minutos;
     segundos = segundos < 10 ? '0' + segundos : segundos;
     relogioDigital.innerHTML = `${corrigirRelogio(horas)}:${corrigirRelogio(minutos)}:${corrigirRelogio(segundos)}`;
 }
-function corrigirRelogio(variavel) {
-    return variavel < 10 ? '0' + variavel : variavel;
+function corrigirRelogio(unidade) {
+    return unidade < 10 ? '0' + unidade : unidade;
 }
