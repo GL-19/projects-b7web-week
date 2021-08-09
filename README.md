030d8ce96d9372a2a9929c9e1a5faf33345a9e1a
GL-19 <loiolagl19@gmail.com>
2021-08-09 00:17:49 -0300

    Adicionado readme e refatoração de código

---
+++ README.md
@@ -0,0 +1,3 @@
+# Relógio Analógico
+
+### Projeto realizado como parte do evento de 7 projetos em 7 dias, organizado pela B7Web
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let ponteiroSegundos = document.querySelector(".p_s");
 let ponteiroMinutos = document.querySelector(".p_m");
 let ponteiroHoras = document.querySelector(".p_h");
 
+atualizarHorario();
 setInterval(atualizarHorario, 1000);
 
 function atualizarHorario() {
@@ -13,16 +14,17 @@ function atualizarHorario() {
     
     relogioDigital.innerHTML = `${corrigirRelogio(horas)}:${corrigirRelogio(minutos)}:${corrigirRelogio(segundos)}`;
 
+    //É necessário atrasar em 90 graus para funcionar corretamente,
+    // pois 0    começa na horizontal(posição 15 minutos) e varia no sentido horário 
     let posicaoHoras = ((360 / 12) * horas) - 90;
     let posicaoMinutos = ((360 / 60) * minutos) - 90;
     let posicaoSegundos = ((360 / 60) * segundos) - 90;
 
-
     ponteiroHoras.style.transform = `rotate(${posicaoHoras}deg)`;
     ponteiroMinutos.style.transform = `rotate(${posicaoMinutos}deg)`;
     ponteiroSegundos.style.transform = `rotate(${posicaoSegundos}deg)`;
-
 }
+
 function corrigirRelogio(unidade) {
     return unidade < 10 ? '0' + unidade : unidade;
 }
