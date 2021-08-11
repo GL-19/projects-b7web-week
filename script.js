3370842e4923b8f46ac7f4d79f944bcc2ea70518
GL-19 <loiolagl19@gmail.com>
2021-08-11 07:00:52 -0300

    Adicionado efeito na barra

--- script.js
+++ script.js
@@ -4,37 +4,41 @@ let rightAnswers = 0;
 showQuestion();
 
 document.querySelectorAll('.options').forEach((element) => {
-    element.addEventListener('click', optionChosen);
+    element.addEventListener('click', optionHasBeenChosen);
 });
 
 document.querySelector('.scoreArea button').addEventListener('click', restartQuiz);
 
-
 function showQuestion() {
+    let rightAnswersPercent = 100 * rightAnswers / questions.length;
+    let answeredQuestionsPercent = 100 * currentQuestion / questions.length;
+    
+    document.querySelector('.progress').style.width = `${answeredQuestionsPercent}%`;
+
     if (questions[currentQuestion]) {
         let q = questions[currentQuestion];
-
+        
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
         document.querySelector('.question').innerHTML = q.question;
 
         let optionsHTML = '';
         for (let i in q.options) {
-            optionsHTML += `<div class="option"><span>${1 + (+i)}</span>${q.options[i]}</div>`;
+            optionsHTML += `<div data-option="${i}" class="option"><span>${1 + (+i)}</span>${q.options[i]}</div>`;
         }
         document.querySelector('.options').innerHTML = optionsHTML;
     } else {
         document.querySelector('.questionArea').style.display = 'none';
         document.querySelector('.scoreArea').style.display = 'block';   
-        document.querySelector('.scorePct').innerHTML = `Acertou ${100 * rightAnswers / questions.length}%`; 
+        document.querySelector('.scorePct').innerHTML = `Acertou ${rightAnswersPercent}%`; 
         document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${rightAnswers} ` 
     }
 }
 
-function optionChosen(event) {
-    let optionNumber = event.target.innerText[0] - 1;
+function optionHasBeenChosen(event) {
+    let chosenOption = parseInt(event.target.getAttribute('data-option'));
     let answer = questions[currentQuestion].answer;
-    if(optionNumber === answer) {
+    if(chosenOption === answer) {
         rightAnswers++;
     }
     currentQuestion++;
