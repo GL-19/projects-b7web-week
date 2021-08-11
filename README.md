ef195a3ed116f5148aea5eb2c014c6003fc96e00
GL-19 <loiolagl19@gmail.com>
2021-08-11 07:22:13 -0300

    Projeto funcionando

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quiz
+
+### Projeto realizado como parte do evento 7 projetos em 7 dias, organizado pela B7Web.
--- script.js
+++ script.js
@@ -10,9 +10,7 @@ document.querySelectorAll('.options').forEach((element) => {
 document.querySelector('.scoreArea button').addEventListener('click', restartQuiz);
 
 function showQuestion() {
-    let rightAnswersPercent = 100 * rightAnswers / questions.length;
     let answeredQuestionsPercent = 100 * currentQuestion / questions.length;
-    
     document.querySelector('.progress').style.width = `${answeredQuestionsPercent}%`;
 
     if (questions[currentQuestion]) {
@@ -21,17 +19,14 @@ function showQuestion() {
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
         document.querySelector('.question').innerHTML = q.question;
-
         let optionsHTML = '';
         for (let i in q.options) {
             optionsHTML += `<div data-option="${i}" class="option"><span>${1 + (+i)}</span>${q.options[i]}</div>`;
         }
         document.querySelector('.options').innerHTML = optionsHTML;
+
     } else {
-        document.querySelector('.questionArea').style.display = 'none';
-        document.querySelector('.scoreArea').style.display = 'block';   
-        document.querySelector('.scorePct').innerHTML = `Acertou ${rightAnswersPercent}%`; 
-        document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${rightAnswers} ` 
+       showResult();
     }
 }
 
@@ -49,4 +44,24 @@ function restartQuiz() {
     currentQuestion = 0;
     rightAnswers = 0;
     showQuestion();
+}
+
+function showResult() {
+    let rightAnswersPercent = 100 * rightAnswers / questions.length;
+    
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.scoreArea').style.display = 'block';   
+    document.querySelector('.scorePct').innerHTML = `Acertou ${rightAnswersPercent}%`; 
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${rightAnswers}`;
+
+    if(rightAnswersPercent <= 40) {
+        document.querySelector('.scoreText1').innerHTML = "Precisa Melhorar";
+        document.querySelector('.scorePct').style.color = "red";
+    } else if( 40 < rightAnswersPercent && rightAnswersPercent < 70) {
+        document.querySelector('.scoreText1').innerHTML = "Muito Bem";
+        document.querySelector('.scorePct').style.color = "yellow";
+    } else {
+        document.querySelector('.scoreText1').innerHTML = "Parabéns";
+        document.querySelector('.scorePct').style.color = "green";
+    }
 }
