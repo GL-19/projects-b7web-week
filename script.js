b7b2b482b050528ae83f937ac972434cc1f977af
GL-19 <loiolagl19@gmail.com>
2021-08-11 06:19:17 -0300

    Adicionada contabilização de respostas corretas

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let currentQuestion = 0;
+let rightAnswers = 0;
 
 showQuestion();
 
 document.querySelectorAll('.options').forEach((element) => {
     element.addEventListener('click', (event) => {
+        optionNumber = event.target.innerText[0] - 1;
+        let answer = questions[currentQuestion].answer;
+        if(optionNumber === answer) {
+            rightAnswers++;
+        }
         currentQuestion++;
-        console.log(event.target.innerHTML);
         showQuestion();
     });
 });
@@ -21,9 +26,14 @@ function showQuestion() {
 
         let optionsHTML = '';
         for (let i in q.options) {
-            optionsHTML += `<div class="option"><span>${i}</span>${q.options[i]}</div>`;
+            optionsHTML += `<div class="option"><span>${1 + (+i)}</span>${q.options[i]}</div>`;
         }
         document.querySelector('.options').innerHTML = optionsHTML;
+    } else {
+        document.querySelector('.questionArea').style.display = 'none';
+        document.querySelector('.scoreArea').style.display = 'block';   
+        document.querySelector('.scorePct').innerHTML = `Acertou ${100 * rightAnswers / questions.length}%`; 
+        document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${rightAnswers} ` 
     }
 }
 
