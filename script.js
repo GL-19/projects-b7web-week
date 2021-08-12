f37189914ec04ce75aa43083099c2b0823015599
GL-19 <loiolagl19@gmail.com>
2021-08-11 21:22:48 -0300

    Projeto funcionando

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const areas = {
+    a: null,
+    b: null,
+    c: null
+};
+
 document.querySelectorAll('.item').forEach((item) => {
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
@@ -35,10 +41,15 @@ function dragLeave(event) {
 function drop(event) {
     event.currentTarget.classList.remove('hover');
     let dragItem = document.querySelector('.item.dragging');
+    let areaIdentifier = event.currentTarget.getAttribute('data-name');
 
     if (event.currentTarget.querySelector('.item') === null) {
         event.currentTarget.appendChild(dragItem);
+
+        areas[areaIdentifier] = dragItem.innerHTML;
+        console.log(areas);
     }
+    checkCombination();
 }
 
 //neutralArea
@@ -55,4 +66,19 @@ function dropNeutral(event) {
     event.currentTarget.classList.remove('hover');
     let dragItem = document.querySelector('.item.dragging');
     event.currentTarget.appendChild(dragItem);
+
+    for(let identifier in areas) {
+        if (areas[identifier] === dragItem.innerHTML) {
+            areas[identifier] = null;
+        }
+    }
+    checkCombination();
+}
+
+function checkCombination() {
+    if(areas.a === '1' && areas.b === '2' && areas.c === '3') {
+        document.querySelector('.areas').className = 'areas correct';
+    } else {
+        document.querySelector('.areas').className = 'areas';
+    }
 }
